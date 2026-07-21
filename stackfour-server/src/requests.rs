240ee78ebbf96bd18badff76497b9b5baf69d7eb
jnavaperez <jvnavaperez@cpp.edu>
2026-07-21 13:51:09 -0700

    changed SQL operations to all be transactions

--- stackfour-server/src/requests.rs
+++ stackfour-server/src/requests.rs
@@ -14,20 +14,23 @@ type DBPool = State<Arc<PgPool>>;
 pub async fn new_game(State(pool): DBPool, Json(payload): Json<NewGameRequest>) -> Result<Json<(GameInfo, Uuid)>,SqlError> {
     println!("received new game request");
 
+    let mut tx = pool.begin().await.unwrap();
+
     if let Some(player_id) = payload.id { // attempt to reconnect if already has a player_id
         let possible_game_id: Option<(Team, Uuid)> = query_as("\
                 SELECT team, game FROM players WHERE player_id = $1\
             ")
             .bind(player_id)
-            .fetch_optional(&*pool).await?;
+            .fetch_optional(&mut *tx).await?;
         if let Some((team, game_id)) = possible_game_id {
             let gstate_sql: Option<GameStateSQL> = query_as("
 SELECT games.* FROM players
 JOIN games ON games.game_id = players.game
 WHERE player_id = $1")
                 .bind(&player_id)
-                .fetch_optional(&*pool).await?;
+                .fetch_optional(&mut *tx).await?;
             if let Some(gstate_sql) = gstate_sql {
+                tx.commit().await?;
                 return Ok(Json((
                    GameInfo {
                         player_id,
@@ -40,7 +43,7 @@ WHERE player_id = $1")
             } else {
                 query("DELETE FROM players WHERE player_id = $1")
                     .bind(player_id)
-                    .execute(&*pool).await?;
+                    .execute(&mut *tx).await?;
             }
         }
     }
@@ -71,7 +74,7 @@ select * FROM created;
 ";// returns a lonely game if it exists, otherwise making a new one (which will itself be lonely)
 
     let gstate_sql = loop {
-        let game:Option<GameStateSQL> = query_as(QUERY).fetch_optional(&*pool).await?;
+        let game:Option<GameStateSQL> = query_as(QUERY).fetch_optional(&mut *tx).await?;
         if let Some(game) = game { break game}
     };
     let (pdata,): (Uuid,) = query_as("
@@ -80,7 +83,9 @@ VALUES ($1, $2)
 RETURNING player_id;")
         .bind(if gstate_sql.lonely {gstate_sql.turn.opposite()} else {gstate_sql.turn})
         .bind(gstate_sql.game_id)
-        .fetch_one(&*pool).await?;
+        .fetch_one(&mut *tx).await?;
+
+    tx.commit().await?;
     Ok(Json((
         GameInfo {
             player_id: pdata,
@@ -95,17 +100,17 @@ RETURNING player_id;")
 pub async fn get_game(Path(player_id):Path<Uuid>, State(pool): DBPool) -> Result<Json<(GameState,Uuid)>,SqlError> {
     println!("received get game request");
     dbg!(&player_id);
-//     let gstate_sql: GameStateSQL = query_as("
-// SELECT games.* FROM players
-// JOIN games ON games.game_id = players.game
-// WHERE player_id = $1")
+    let mut tx = pool.begin().await?;
+
     let gstate_sql: GameStateSQL = query_as("
 UPDATE games
 SET last_accessed = now()
 FROM players WHERE players.player_id = $1 AND games.game_id = players.game
 RETURNING games.*")
         .bind(&player_id)
-        .fetch_one(&*pool).await?;
+        .fetch_one(&mut *tx).await?;
+
+    tx.commit().await?;
     Ok(Json((gstate_sql.into_gamestate(),*INSTANCE_ID)))
 }
 
@@ -114,16 +119,19 @@ pub async fn drop_piece(Path(player_id):Path<Uuid>, State(pool): DBPool, Json(pa
     if payload.column >= COLUMNS {
         return Err((StatusCode::BAD_REQUEST, "Column requested exceeds max amount of columns".to_string()));
     }
+
+    let mut tx = pool.begin().await.map_err(|e|(StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
     let (team, game_id): (Team, Uuid) = query_as("
 SELECT team, game FROM players
 WHERE player_id = $1")
         .bind(player_id)
-        .fetch_one(&*pool).await.map_err(|e|(StatusCode::BAD_REQUEST, e.to_string()))?;
+        .fetch_one(&mut *tx).await.map_err(|e|(StatusCode::BAD_REQUEST, e.to_string()))?;
     let mut gstate: GameState = query_as::<_,GameStateSQL>("
 SELECT * FROM games
 WHERE game_id = $1")
         .bind(game_id)
-        .fetch_one(&*pool).await.map_err(|e|(StatusCode::BAD_REQUEST, e.to_string()))?.into_gamestate();
+        .fetch_one(&mut *tx).await.map_err(|e|(StatusCode::BAD_REQUEST, e.to_string()))?.into_gamestate();
     if team != gstate.turn {
         return Err((StatusCode::BAD_REQUEST, "Team of chip does not match whose turn it currently is".to_string()));
     }
@@ -155,13 +163,16 @@ RETURNING *")
             Winner::Blue { winning_chips: c } => Some(*array_2d_into_1d(&c)),
             Winner::Red { winning_chips: c } => Some(*array_2d_into_1d(&c)),
         })
-        .fetch_one(&*pool).await.map_err(|e|(StatusCode::BAD_REQUEST, e.to_string()))?;
+        .fetch_one(&mut *tx).await.map_err(|e|(StatusCode::BAD_REQUEST, e.to_string()))?;
 
+    tx.commit().await.map_err(|e|(StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
     Ok(Json((updated_gstate_sql.into_gamestate(),*INSTANCE_ID)))
 }
 
 pub async fn restart_game(Path(player_id):Path<Uuid>, State(pool): DBPool) -> Result<Json<(GameState,Uuid)>,SqlError> {
     println!("recieved restart game request");
+
+    let mut tx = pool.begin().await?;
 
     let gstate_sql: GameStateSQL = query_as("
 UPDATE games SET
@@ -177,7 +188,8 @@ WHERE game_id = (
 )
 RETURNING *;")
         .bind(player_id)
-        .fetch_one(&*pool).await?;
+        .fetch_one(&mut *tx).await?;
 
+    tx.commit().await?;
     Ok(Json((gstate_sql.into_gamestate(),*INSTANCE_ID)))
 }
